417837f6172c1e985f1c0df9b70c061d2c1523db
fojletuhin <fojletuhin@gmail.com>
2023-09-19 20:12:31 +0600

    first-commit

---
+++ Tuhin.js
@@ -0,0 +1,26 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
+
+
+const goTopBtn = document.querySelector('.up');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+    if(window.scrollY < 250)
+    {
+        goTopBtn.style.display ="none"
+    }
+    else{
+        goTopBtn.style.display="flex"
+    }
+}
+
+
